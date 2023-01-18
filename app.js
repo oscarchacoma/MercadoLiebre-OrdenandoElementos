306e326002d4582f62b67d230d5da54344fa3d1f
OscarChacoma <ochacoma2.0@gmail.com>
2023-01-18 17:12:46 -0300

    Practica integradora MercadoLiebre - Organizando elementos

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ("express");
+const path = require("path");
+const app = express(); 
+
+app.use(express.static("public")); 
+
+
+app.listen(3030, ()=> console.log("El servidor está funcionando en el puerto 3030"))//liten recive dos parametros, el puerto y una funcion colback
+
+app.get("/",(req,res)=>{
+    return res.sendFile(path.join(__dirname,"/views/home.html"))
+})
+
+app.get("/register",(req,res)=>{
+    return res.sendFile(path.join(__dirname,"./views/register.html"))
+})
+
+app.get("/login",(req,res)=>{
+    return res.sendFile(path.join(__dirname,"./views/login.html"))
+})
